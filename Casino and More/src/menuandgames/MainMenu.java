c5d6b2f860290cc3148e1ee1c5e6415cb43cda8d
ajt18c <60716244+ajt18c@users.noreply.github.com>
2020-02-10 17:16:06 -0500

    Add files via upload

--- Casino and More/src/menuandgames/MainMenu.java
+++ Casino and More/src/menuandgames/MainMenu.java
@@ -15,11 +15,13 @@ private JButton spit;
 private JButton slapjack;
 private JButton roulette;
 Image background = new ImageIcon("res/MainMenuBackground.jpg").getImage();
+Image CasinoAndMoreLogo = new ImageIcon("res/CasinoAndMoreLogo.png").getImage();
 
 @Override
     public void paintComponent(Graphics g){
         g.clearRect(0,0, getWidth(), getHeight());
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        g.drawImage(CasinoAndMoreLogo, 600, 50, 720, 300, this);
     }
 
 public MainMenu(){
@@ -27,7 +29,6 @@ public MainMenu(){
     Image tempSetting = new ImageIcon("res/SettingsButton.png").getImage();
     Image tempSettingImg = tempSetting.getScaledInstance(50,50,java.awt.Image.SCALE_SMOOTH);
     Icon settingsButton = new ImageIcon(tempSettingImg);
-
     this.setLayout(null);
     settings = new JButton(settingsButton);
     this.add(settings);
@@ -36,39 +37,62 @@ public MainMenu(){
     settings.setContentAreaFilled(false);
     settings.setFocusPainted(false);
 
+
     //how to play / information button
     Image tempHowTo = new ImageIcon("res/InformationHowToIcon.jpg").getImage();
-    Image tempHowToImg = tempHowTo.getScaledInstance(50,50,java.awt.Image.SCALE_SMOOTH);
+    Image tempHowToImg = tempHowTo.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
     Icon howToButton = new ImageIcon(tempHowToImg);
-
     howTo = new JButton(howToButton);
     this.add(howTo);
-    howTo.setBounds(250, 250, 50, 50);
+    howTo.setBounds(85, 25, 50, 50);
     howTo.setOpaque(false);
     howTo.setContentAreaFilled(false);
     howTo.setFocusPainted(false);
 
-
-
-    blackjack = new JButton("");
+    //blackjack logo button
+    Image tempBlackjack = new ImageIcon("res/BlackJackMenuLogo.png").getImage();
+    Image tempBlackjackImg = tempBlackjack.getScaledInstance(400,175,java.awt.Image.SCALE_SMOOTH);
+    Icon blackjackButton = new ImageIcon(tempBlackjackImg);
+    blackjack = new JButton(blackjackButton);
     this.add(blackjack);
-    blackjack.setBounds(50, 50, 50, 50);
+    blackjack.setBounds(250, 400, 400, 175);
+    blackjack.setFocusPainted(false);
 
-    solitaire = new JButton("");
+    //solitaire logo button
+    Image tempSolitaire = new ImageIcon("res/SolitaireMenuLogo.png").getImage();
+    Image tempSolitaireImg = tempSolitaire.getScaledInstance(400,175,java.awt.Image.SCALE_SMOOTH);
+    Icon solitaireButton = new ImageIcon(tempSolitaireImg);
+    solitaire = new JButton(solitaireButton);
     this.add(solitaire);
-    solitaire.setBounds(50, 50, 50, 50);
+    solitaire.setBounds(250, 650, 400, 175);
+    solitaire.setFocusPainted(false);
 
-    spit = new JButton("");
+
+    Image tempSpit = new ImageIcon("res/SpitMenuLogo.png").getImage();
+    Image tempSpitImg = tempSpit.getScaledInstance(400,175,java.awt.Image.SCALE_SMOOTH);
+    Icon SpitButton = new ImageIcon(tempSpitImg);
+    spit = new JButton(SpitButton);
     this.add(spit);
-    spit.setBounds(50, 50, 50, 50);
+    spit.setBounds(750, 400, 400, 175);
+    spit.setFocusPainted(false);
 
-    slapjack = new JButton("");
+    
+    Image tempSlapjack = new ImageIcon("res/SlapJackMenuLogo.jpg").getImage();
+    Image tempSlapjackImg = tempSlapjack.getScaledInstance(400,175,java.awt.Image.SCALE_SMOOTH);
+    Icon SlapJackButton = new ImageIcon(tempSlapjackImg);
+    slapjack = new JButton(SlapJackButton);
     this.add(slapjack);
-    slapjack.setBounds(50, 50, 50, 50);
+    slapjack.setBounds(750, 650, 400, 175);
+    slapjack.setFocusPainted(false);
 
-    roulette = new JButton("");
+
+    Image tempRoulette = new ImageIcon("res/RouletteMenuLogo.png").getImage();
+    Image tempRouletteImg = tempRoulette.getScaledInstance(400,150,java.awt.Image.SCALE_SMOOTH);
+    Icon RouletteButton = new ImageIcon(tempRouletteImg);
+    roulette = new JButton(RouletteButton);
     this.add(roulette);
-    roulette.setBounds(50, 50, 50, 50);
+    roulette.setBounds(1250, 525, 400, 175);
+    roulette.setFocusPainted(false);
 }
 
 }
