7131244415c509e41f9dc6223edf13f7aefe8131
Apple-Minion <46580965+Apple-Minion@users.noreply.github.com>
2020-02-16 15:21:20 -0500

    Added card images and a player class

--- Casino and More/src/menuandgames/BlackJack.java
+++ Casino and More/src/menuandgames/BlackJack.java
@@ -2,15 +2,36 @@
 package menuandgames;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class BlackJack extends JPanel{
 
     private Deck deck;
 
-    BlackJack() {
-        deck = new Deck();
+    private JButton hit;
+    private JButton stand;
+    private JLabel playerStatus;
+    private JLabel dealerStatus;
+    private Player[] players;
 
-        
+    BlackJack(int numPlayers) {
+        this.setLayout(null);
+        deck = new Deck();
+        players = new BJPlayer[numPlayers];
+
+        hit = new JButton("HIT");
+	    //hit.setBounds(400, 310, 150, 75);
+		//hit.addActionListener(new hitListener());
+        this.add(hit);
+
+        stand = new JButton("STAND");
+	    //stand.setBounds(600, 310, 150, 75);
+		//hit.addActionListener(new standListener());
+        this.add(stand);
+
+        playerStatus = new JLabel("");
+        dealerStatus = new JLabel("");
     }
 
     public void newGame() {
--- Casino and More/src/menuandgames/CasinoAndMore.java
+++ Casino and More/src/menuandgames/CasinoAndMore.java
@@ -11,6 +11,10 @@ public CasinoAndMore(){
     //setting up background
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setSize(FRAME_WIDTH, FRAME_HEIGHT);
+    // To make full screen
+    // Add to settings?
+    //setExtendedState(JFrame.MAXIMIZED_BOTH); 
+    //setUndecorated(true);
     setLocationRelativeTo(null);
     setTitle("Casino and More");
 
