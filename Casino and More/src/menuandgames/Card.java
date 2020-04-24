ce8ba93d155f57eeee6e591492f45dab0f0b75fc
ajt18c <ajt18c@my.fsu.edu>
2020-04-24 14:14:25 -0400

    Revert "Card class changed"
    This reverts commit fc1e8f378d0131ede6bd734e5f109252f789055d.

--- Casino and More/src/menuandgames/Card.java
+++ Casino and More/src/menuandgames/Card.java
@@ -1,45 +1,50 @@
-package src.menuandgames;
+
+package menuandgames;
+
 import java.awt.Image;
 import javax.swing.ImageIcon;
 // The cards in the deck
 public class Card {
-    private static Image back = new ImageIcon("res/cards/backs/StockCardBack.png").getImage();
-	public enum Suit {SPADES, HEARTS, CLUBS, DIAMONDS}
+    // The card back
+    private static Image back = new ImageIcon("Casino and More/res/cards/backs/Red.png").getImage();
+	private enum Suit {SPADES, HEARTS, CLUBS, DIAMONDS}
 	private int val;
     private Suit suit;
     private Image img;
-    
-    
+	
 	public Card(int num, int suit) {
+        // Sets up the card with a suit, number, and image
         val = num + 1;
         switch(suit){
             case 0:
                 this.suit = Suit.SPADES;
-                img = new ImageIcon("res/cards/" + String.valueOf(val) + "spade.png").getImage();
+                img = new ImageIcon("Casino and More/res/cards/" + String.valueOf(val) + "spade.png").getImage();
                 break;
             case 1:
                 this.suit = Suit.HEARTS;
-                img = new ImageIcon("res/cards/" + String.valueOf(val) + "heart.png").getImage();
+                img = new ImageIcon("Casino and More/res/cards/" + String.valueOf(val) + "heart.png").getImage();
                 break;
             case 2:
                 this.suit = Suit.CLUBS;
-                img = new ImageIcon("res/cards/" + String.valueOf(val) + "club.png").getImage();
+                img = new ImageIcon("Casino and More/res/cards/" + String.valueOf(val) + "club.png").getImage();
                 break;
             case 3:
                 this.suit = Suit.DIAMONDS;
-                img = new ImageIcon("res/cards/" + String.valueOf(val) + "diamond.png").getImage();
-            
+                img = new ImageIcon("Casino and More/res/cards/" + String.valueOf(val) + "diamond.png").getImage();
         }
 	}
     
+    // Sets a new card back
     public static void setBack(String imgLocation) {
         back = new ImageIcon(imgLocation).getImage();
     }
 
+    // returns the value of the card
 	public int getVal() {
 		return val;
     }
     
+    // Returns the name of the card
     public String getName() {
         switch(val) {
             case 1:
@@ -54,29 +59,19 @@ public class Card {
                 return String.valueOf(val);
         }
     }
-	
+    
+    // Returns the suit of the card
 	public Suit getSuit() {
         return suit;
     }
     
+    // Returns the card image
     public Image getImg() {
         return img;
     }
 
+    // Returns the image for the card back
     public static Image getBack() {
         return back;
     }
-
-    public int getcolornumber(){
-        if(this.suit == Suit.HEARTS || this.suit == Suit.DIAMONDS){
-            return 1;
-        }
-        else 
-            return 2;
-    }
-
-    
 }
-
-//button.setactioncommand
-//e.getAction
